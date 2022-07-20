c16fc2ee0eacb5e0ed327ec5b338fb6a7665c466
asjjun <asjjun@naver.com>
2022-07-21 00:35:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -261,3 +261,19 @@ window.onload=function(){ // 페이지 로드 시 첫 실행 함수
     }
 }
 
+/* ================================================================================= */
+const bookBtn = document.querySelector('.bookmark-btt');
+const section = document.querySelector('section');
+const label = document.querySelector('label');
+
+// 클릭시 사이드 메뉴 생성, 북마크 버튼 사라짐
+bookBtn.addEventListener('click', function(){
+    section.style.left = 0;
+    label.style.opacity = 0;
+})
+
+// 포커스 아웃 시 사이드 메뉴 사라짐, 북마크 버튼 생성
+bookBtn.addEventListener('blur', function(){
+    section.style.left = '-200px';
+    label.style.opacity = 1;
+})
